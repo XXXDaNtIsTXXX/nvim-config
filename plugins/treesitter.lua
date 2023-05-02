9f828f72712b0311711d7674defbc04aa6f5993b
Morozov Sergey <morozovsa@melonfashion.com>
2023-05-02 15:00:43 +0300

    update

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -2,9 +2,10 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
-    if not opts.ensure_installed then opts.ensure_installed = {} end
+    if not opts.ensure_installed then opts.ensure_installed = { "python" } end
     require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
+      "lua",
+      "python",
     })
   end,
 }
